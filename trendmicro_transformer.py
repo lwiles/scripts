1204a5cb2a9946518868c5b03a4c936a43056922
Liam Wiles <liam.wiles@bazaarvoice.com>
2013-12-12 11:24:06 -0600

    trendmicro_transformer.py

---
+++ trendmicro_transformer.py
@@ -0,0 +1,66 @@
+import csv
+import xml.etree.ElementTree as ET
+
+###############################################
+# Travelocity Standard Client Feed Transformer
+###############################################
+
+# file definitions
+source_file = 'blank.xml'
+output_file = 'native_content.xml'
+
+#namespace declaration
+namespace = '{http://www.bazaarvoice.com/xs/PRR/StandardClientFeed/5.6}'
+ET.register_namespace('', 'http://www.bazaarvoice.com/xs/PRR/StandardClientFeed/5.6')
+
+# parse the source standard client feed
+tree = ET.parse(source_file)
+root = tree.getroot()
+
+reviews = dict()
+reviewCounter = 1
+profileCounter = 1
+
+with open('reviewsReport.csv', 'rb') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in csvreader:
+        externalid = row[1]
+        if externalid in reviews:
+            reviews[externalid].append(row)
+        else:
+            reviews[externalid] = list()
+            reviews[externalid].append(row)
+
+for product in reviews:
+    ProductElement = ET.SubElement(root, '{0}Product'.format(namespace), attrib={'id': product})
+    ProductExternalId = ET.SubElement(ProductElement, '{0}ExternalId'.format(namespace))
+    ProductExternalId.text = product
+    Reviews = ET.SubElement(ProductElement, '{0}Reviews'.format(namespace))
+    for review in reviews[product]:
+        Review = ET.SubElement(Reviews, '{0}Review'.format(namespace), attrib={'id': str(reviewCounter)})
+        reviewCounter += 1
+        SubmissionTime = ET.SubElement(Review, '{0}SubmissionTime'.format(namespace))
+        SubmissionTime.text = review[3].replace('/', '-') + 'T12:00:00.000-00:00'
+        Rating = ET.SubElement(Review, '{0}Rating'.format(namespace))
+        Rating.text = review[2].strip()
+        Title = ET.SubElement(Review, '{0}Title'.format(namespace))
+        Title.text = review[0].decode('utf-8')
+        ReviewText = ET.SubElement(Review, '{0}ReviewText'.format(namespace))
+        ReviewText.text = review[4].decode('utf-8')
+        if review[5]:
+            # print(review[5].strip().replace(' ', ''))
+            UserProfileReference = ET.SubElement(Review, '{0}UserProfileReference'.format(namespace), attrib={'id': str(profileCounter)})
+            ProfileExternalId = ET.SubElement(UserProfileReference, '{0}ExternalId'.format(namespace))
+            profileCounter += 1
+            DisplayName = ET.SubElement(UserProfileReference, '{0}DisplayName'.format(namespace))
+            DisplayName.text = review[5].strip().replace(' ', '').decode('utf-8')
+            Anonymous = ET.SubElement(UserProfileReference, '{0}Anonymous'.format(namespace))
+            Anonymous.text = 'false'
+            HyperlinkingEnabled = ET.SubElement(UserProfileReference, '{0}HyperlinkingEnabled'.format(namespace))
+            HyperlinkingEnabled.text = 'false'
+        else:
+            print('false')
+
+
+
+tree.write(output_file, encoding="utf-8", xml_declaration=True, default_namespace='')
