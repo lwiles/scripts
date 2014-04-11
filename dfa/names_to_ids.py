b043541fe97061f0cf8b1f3f55e095d3ca588c9f
Liam Wiles <liam.wiles@dimensional.com>
2014-04-11 10:50:38 -0500

    comments added to names_to_ids.py

--- dfa/names_to_ids.py
+++ dfa/names_to_ids.py
@@ -6,11 +6,16 @@ import os
 
 DFA_dict = dict()
 
+# open employee CSV file and create a dictionary of IDs (keys)
+# to Firstname, Lastname tuple (values)
 with open('employees_and_ids.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     for row in csvreader:
         DFA_dict[row[2]] = (row[1].lower(), row[0].lower())
 
+# in the current directory, loop through the filenames and
+# attempt to match both first and last name, replacing with
+# employee ID
 for filename in os.listdir('.'):
     lowercase = filename.lower()
     for key, value in DFA_dict.items():
