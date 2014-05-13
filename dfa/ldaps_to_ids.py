f778f87fd66e59a46a825355a0b5491a0bc9d52b
Liam Wiles <liam.wiles@dimensional.com>
2014-05-13 11:54:52 -0500

    converts everything to lowercase

--- dfa/ldaps_to_ids.py
+++ dfa/ldaps_to_ids.py
@@ -8,14 +8,15 @@ DFA_dict = dict()
 
 # open employee CSV file and create a dictionary of LDAPs (keys)
 # to employee IDs (values)
-with open('employees_and_ids.csv', 'r') as csvfile:
+with open('ldap_to_id.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     for row in csvreader:
-        DFA_dict[row[0]] = row[1]
+        DFA_dict[row[0].lower()] = row[1]
 
 # in the current directory, loop through the filenames and
 # attempt to match LDAP, replacing with employee ID
 for filename in os.listdir('.'):
+    lowercase = filename.lower().strip('.png')
     for key, value in DFA_dict.items():
-        if key in filename:
+        if key == lowercase:
             os.rename(filename, value + '.png')
