28e491013aecd94306bdad79892aa2121aeb8be6
Liam Wiles <liam.wiles@dimensional.com>
2014-06-11 10:40:16 -0500

    added exception handling and altered script to use 'os.path.splitext'

--- dfa/ldaps_to_ids.py
+++ dfa/ldaps_to_ids.py
@@ -11,12 +11,20 @@ DFA_dict = dict()
 with open('ldap_to_id.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     for row in csvreader:
-        DFA_dict[row[0].lower()] = row[1]
+        try:
+            DFA_dict[row[0].lower()] = row[1]
+        except Exception as e:
+            print(e)
 
 # in the current directory, loop through the filenames and
 # attempt to match LDAP, replacing with employee ID
-for filename in os.listdir('.'):
-    lowercase = filename.lower().strip('.png')
+for myfile in os.listdir('.'):
+    lowercase = myfile.lower()
+    filename = os.path.splitext(lowercase)
+    ldap = filename[0]
     for key, value in DFA_dict.items():
-        if key == lowercase:
-            os.rename(filename, value + '.png')
+        if key == ldap:
+            try:
+                os.rename(myfile, value + '.png')
+            except Exception as e:
+                print(e)
